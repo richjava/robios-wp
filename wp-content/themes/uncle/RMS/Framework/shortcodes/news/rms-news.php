9758591f8bf6204c7236bf510480d99fc804ffd4
Richard Lovell <richjavalabs@gmail.com>
2015-04-14 23:55:46 -0700

    Refactoring

--- wp-content/themes/uncle/RMS/Framework/shortcodes/news/rms-news.php
+++ wp-content/themes/uncle/RMS/Framework/shortcodes/news/rms-news.php
@@ -1,47 +1,109 @@
+<!--<div class="vc_row wpb_row vc_row-fluid">
+	<div class="vc_col-sm-4 wpb_column vc_column_container ">
+		<div class="wpb_wrapper">
+			<h4 style="margin: ; width: 100%; color: ; font-size: ;">Quickly &nbsp; <i class="fa fa-rocket text-color"></i> &nbsp; Qualitatively Work</h4><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ornare sem sit amet mauris bibendum, ac fringilla quam eleifend. Praesent elit tellus, venenatis in lorem ac, porta accumsan sapien. Cras malesuada feugiat .</p>
+		</div> 
+	</div> 
+
+	<div class="vc_col-sm-4 wpb_column vc_column_container ">
+		<div class="wpb_wrapper">
+			<h4 style="margin: ; width: 100%; color: ; font-size: ;">Large portfolio of &nbsp; <i class="fa fa-suitcase text-color"></i> &nbsp; works</h4><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ornare sem sit amet mauris bibendum, ac fringilla quam eleifend. Praesent elit tellus, venenatis in lorem ac, porta accumsan sapien. Cras malesuada feugiat .</p>
+		</div> 
+	</div> 
+
+	<div class="vc_col-sm-4 wpb_column vc_column_container ">
+		<div class="wpb_wrapper">
+			<h4 style="margin: ; width: 100%; color: ; font-size: ;">10 years of successful  &nbsp; <i class="fa fa-trophy text-color"></i> &nbsp; business</h4><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ornare sem sit amet mauris bibendum, ac fringilla quam eleifend. Praesent elit tellus, venenatis in lorem ac, porta accumsan sapien. Cras malesuada feugiat .</p>
+		</div> 
+	</div> 
+</div>-->
 <?php
 function news($atts)
 {
     extract(shortcode_atts(array(
-        'number'       => '6'
+        'no_of_posts'       => '4'
     ), $atts));
     
-    $fa = '';
-        $features = array(
-            'post_type'     => array('news'),
-            'post_status'   => array('publish'),
-            'orderby'       => 'date',
-            'order'         => 'DESC',
-            'posts_per_page'=> $number                      
+    $team_return = '';
+    
+        $args1 = array(
+            'post_type'         => array('news'),
+            'post_status'       => array('publish'),
+            'posts_per_page'    => $no_of_posts,
+            'orderby'           => 'date',
+            'order'             => 'DESC'
         );
         
-        query_posts($features);
+        query_posts($args1);
         if(have_posts())
         {
-            while (have_posts())
+//			$team_return .= 
+//					'<p>
+//						<a id="news-prev" href=""><i class="fa fa-chevron-left icon arrow-next outline attached"></i></a>
+//						<a id="news-next" href=""><i class="fa fa-chevron-right icon arrow-prev outline attached"></i></a>
+//					</p>';
+//            $team_return .= '<div class="list_carousel"><ul id="news-slider">';
+            $team_return .= '<div class="vc_row wpb_row vc_row-fluid">';
+            while(have_posts())
             {
                 the_post();
-                $icon = get_post_meta(get_the_ID(), 'newsicon', TRUE);
-                if($icon != '')
-                {
-                    $ic = $icon;
-                }
-                else
-                {
-                    $ic = 'fa fa-pencil-square-o';
-                }
-                $fa .= '<div class="four columns own_gap">
-							<p><a href="'.get_permalink().'"><i class="'.$ic.' icon huge"></i></a></p>
-							<h4>'.substr(get_the_title(), 0, 25).'</h4>
-							<p>'.substr(get_the_content(), 0, 300).'</p>
-                        </div>';
+                $des1 = get_post_meta(get_the_ID(), 'designation', TRUE);
+				if($des1 != '')
+				{
+					$desig = $des1;
+				}
+				else
+				{
+					$desig = '';
+				}
+				if(has_post_thumbnail( get_the_ID() ))
+				{
+					$team_thumb = get_the_post_thumbnail(get_the_ID(), 'news_thumb');
+				}
+				else
+				{
+					$team_thumb = '<img src="http://placehold.it/360x270" alt="Reorder">';
+				}
+                                $team_return .='<div class="vc_col-sm-4 wpb_column vc_column_container ">
+		<div class="wpb_wrapper">
+			<h4 style="margin: ; width: 100%; color: ; font-size: ;">'.get_the_title().'</h4><p>'.substr(get_the_content(), 0, 150).'</p>
+		</div> 
+	</div>';
+//			   $team_return .='
+//				<li class="news-item">
+//					<i>'.$team_thumb.'</i>
+//					<p></p>
+//					<h4>'.get_the_title().'</h4>
+//					<p></p>
+//					<p>'.substr(get_the_content(), 0, 150).'</p>
+//					<h4 class="text-subtitle">From <b>'.$desig.'</b></h4>
+//					<ul>';
+//						$fa = get_post_meta(get_the_ID(), 'twitter', true); if($fa != '') { 
+//							$team_return .='<li>'.$fa.'</li>';
+//						} 
+//						$gp = get_post_meta(get_the_ID(), 'linkedin', true); if($gp != '') { 
+//							$team_return .='<li>'.$gp.'</li>';
+//						}
+//						$tw = get_post_meta(get_the_ID(), 'google', true); if($tw != '') { 
+//							$team_return .='<li>'.$tw.'</li>';
+//						}
+//					$team_return .='</ul>
+//					<p><a href="#" class="button outline">Order service</a></p>
+//				</li>';
             }
+			//$team_return .= '</ul></div>';
+            $team_return .= '</div>';
+			
         }
         else
         {
-            $fa .= '<div class="container"><div class="row"><div class="col-lg-12"><h1 class="common_heading">Please insert some news first.</h1></div></div></div>';
+            $team_return .= '<div class="col-lg-12 text-center">';
+                $team_return .= '<h1 class="common_main_heading">Please Insert Some <span>News</span> First.</h1>';
+            $team_return .= '</div>';
         }
         wp_reset_query();
-        return $fa;
     
+    
+    return $team_return;
 }
-add_shortcode( "news", "news" );
+add_shortcode( "rms-news", "news" );
